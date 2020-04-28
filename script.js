7dba2701d5781a015623bd6890e925a13856e1ab
Ali <alimkali.alizhan@gmail.com>
2020-04-28 18:05:41 +0600

    16x16 grid flexible for container

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16**2; i++) {
-    let divOfGrid = document.createElement('div');
-    container.appendChild(divOfGrid);
-    console.log(i);
+for (let i = 0; i < 16; i++) {
+    let containerOfDiv = document.createElement('div');
+    containerOfDiv.classList.add('containerOfDiv');
+    for (let j = 0; j < 16; j++) {
+        let divOfGrid = document.createElement('div');
+        divOfGrid.classList.add('divOfGrid');
+        containerOfDiv.appendChild(divOfGrid);
+    }
+    container.appendChild(containerOfDiv);
 }
